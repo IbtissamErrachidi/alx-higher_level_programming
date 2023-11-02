5ead8b718f2cd07f17538a71c7371fe9b65b3727
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-11-02 21:08:17 +0000

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
-
-    args = sys.argv[1:]
-    total = sum(map(int, args))
-
-    print("Somme des arguments : {}".format(total))
-
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
