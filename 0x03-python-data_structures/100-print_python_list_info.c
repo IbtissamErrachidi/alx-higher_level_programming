30f1bfbe9c2679b4a74debfba8e08d305c7d5dc2
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-11-04 23:34:50 +0000

    100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,49 +1,15 @@
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
 
-void print_python_list_info(PyObject *p) {
-    if (PyList_Check(p)) {
-        Py_ssize_t size = PyList_GET_SIZE(p);
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
 
-        printf("[*] Size of the Python List = %ld\n", size);
-
-        PyListObject *list = (PyListObject *)p;
-        printf("[*] Allocated = %ld\n", list->allocated);
-
-        for (Py_ssize_t i = 0; i < size; i++) {
-            PyObject *element = PyList_GET_ITEM(p, i);
-            printf("Element %ld: %s\n", i, Py_TYPE(element)->tp_name);
-        }
-    } else {
-        PyErr_SetString(PyExc_TypeError, "Object is not a Python list");
-    }
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
-
-static PyObject *print_info(PyObject *self, PyObject *args) {
-    PyObject *py_list;
-
-    if (!PyArg_ParseTuple(args, "O", &py_list)) {
-        return NULL;
-    }
-
-    print_python_list_info(py_list);
-
-    Py_RETURN_NONE;
-}
-
-static PyMethodDef methods[] = {
-    {"print_info", print_info, METH_VARARGS, "Print information about a Python list."},
-    {NULL, NULL, 0, NULL}
-};
-
-static struct PyModuleDef module = {
-    PyModuleDef_HEAD_INIT,
-    "listinfo",
-    "Python List Info Module",
-    -1,
-    methods
-};
-
-PyMODINIT_FUNC PyInit_listinfo(void) {
-    return PyModule_Create(&module);
-}
-
