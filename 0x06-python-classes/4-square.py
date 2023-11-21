6b3f2861f2f5b5bf4d3775f96043c5bfad81a8e0
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-11-21 14:55:43 +0000

    4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" define a class Square"""
+
+
+class Square():
+    """ the class square """
+    def __init__(self, __size=0) -> None:
+        if type(__size) != int:
+            raise TypeError("size must be an integer")
+        elif __size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = __size
+
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+
+    def area(self):
+        return (self.__size * self.__size)
