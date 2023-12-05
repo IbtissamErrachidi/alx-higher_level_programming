727506a78ceba8abc66daae5f371b7a71202451e
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-12-05 21:37:54 +0000

    Adding 012

--- 0x0B-python-input_outpu/1-write_file.py
+++ 0x0B-python-input_outpu/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """function write_file"""
 
+
 def write_file(filename="", text=""):
     """return the number the char inserted"""
     with open(filename, 'w', encoding='utf-8') as f:
