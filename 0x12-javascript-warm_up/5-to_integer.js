214e7618522fa601f0b472449dd55538c757e015
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-02-13 00:05:45 +0000

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,8 @@
 const arg = process.argv[2]; 
 
 if (!isNaN(arg)) {
-    console.log(`My number: ${Number(arg)}`);
+    console.log(`My number: ${Math.floor(Number(arg))}`);
 } else {
     console.log("Not a number");
 }
+
