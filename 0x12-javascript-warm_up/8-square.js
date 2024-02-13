e57d08df0af12fa1681ecd911fc37033af6103b6
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-02-13 13:05:35 +0000

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
+
+
+
 const argument = process.argv[2];
 const converted = parseInt(argument);
 
 if (!isNaN(converted)) {
     for (let i = 0; i < converted; i++) {
+        let str = '';
         for (let j = 0; j < converted; j++) {
-            console.log('x'); // Utilisez process.stdout.write pour imprimer sans ajouter de nouvelle ligne
+            str += 'X';
         }
-        console.log(); // Nouvelle ligne après chaque ligne d'x
+        console.log(str);
     }
-} else { // Utilisez '===' pour comparer le type de variable
+} else {
     console.log('Missing size');
 }
+
