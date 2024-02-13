80b5e899ca15d7f831d163b50dca8c818cdd5ecd
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-02-13 13:18:04 +0000

    task9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const first = process.argv[2];
+const convert1 = parseInt(first);
+const second = process.argv[3];
+const convert2 = parseInt(second);
+function add(a, b){
+    a = convert1;
+    b = convert2;
+    if(isNaN(a) || isNaN(b)){
+        console.log('NaN');
+    }
+    else{
+        console.log(a+b)
+    }
+}
+
+add(convert1,convert2);
