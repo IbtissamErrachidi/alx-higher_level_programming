f8bbcfc64893d2ff7fecbc56af50291ca4963c12
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-02-13 12:43:34 +0000

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,13 +6,11 @@ const converted = parseInt(argument);
 if (!isNaN(converted)) {
     for (let i = 0; i < converted; i++) {
         for (let j = 0; j < converted; j++) {
-            process.stdout.write('x');
+            process.stdout.write('x'); // Utilisez process.stdout.write pour imprimer sans ajouter de nouvelle ligne
         }
-        console.log(); 
+        console.log(); // Nouvelle ligne après chaque ligne d'x
     }
-} else if (typeof(argument) === 'string') { 
-    console.log('Missing size');
-} else {
-    return;
+} else if (typeof(argument) === 'string') { // Utilisez '===' pour comparer le type de variable
+    process.stdout.write('Missing size');
 }
 
