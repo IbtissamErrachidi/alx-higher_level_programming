82f482a9c3a0c423c3acb33ee4da4f10330d2636
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-02-14 15:43:40 +0000

    task3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
+
 class Rectangle {
-   constructor (w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-    print() {
-        for (let i = 0; i < this.height; i++) {
-	    let str = '';
-            for (let j = 0; j < this.width; j++) {
-                str += 'X';
-            }
-            console.log(str);
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
     }
+  }
 }
-
 module.exports = Rectangle;
