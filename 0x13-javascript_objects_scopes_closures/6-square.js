772f315269be46849d3858a5814ef0f24c5d3479
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-02-14 16:51:37 +0000

    task6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const _Square = require('./5-square');
+
+class Square extends Rectangle {
+    constructor(size){
+        super(size,size);
+    }
+    charPrint(c){
+        if(c){
+            for (let i = 0; i < this.height; i++) {
+                for (let j = 0; j < this.width; j++) {
+                    process.stdout.write(c);
+                }
+                console.log();
+            }
+        }
+        else{
+            this.print();
+        }
+    }
+    
+}
+
+module.exports = Square;
