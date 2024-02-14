36f9aadc7ec4806d0fb029188556ecdfa0276f01
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-02-14 17:03:41 +0000

    task6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,14 @@
-#!/usr/bin/node
-
-const _Square = require('./5-square');
+t _Square = require('./5-square');
 
 class Square extends _Square {
-    charPrint(c){
-            if (typeof c === 'undefined') {
-              this.print();
-            } else {
-                for (let i = 0; i < this.height; i++) {
-                    str = '';
-                    for (let j = 0; j < this.width; j++) {
-                        str += c;
-                    }
-                    console.log(str);
-                }
-    
-            }
-    } 
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
 }
-
 module.exports = Square;
