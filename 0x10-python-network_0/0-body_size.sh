bda3dd5eb950a7f014439de20286b6cc86541c2b
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-02-27 21:53:33 +0000

    body-size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Bash script to get the body size of a request
-size=$(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}')
-echo "Size of the body: $size bytes"
-
+# script to get the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '| awk '{print $2}'
