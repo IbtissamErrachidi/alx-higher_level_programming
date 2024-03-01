bb7b9b5d43bae237169eaa7431d8b6d75812f9b1
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-01 22:06:57 +0000

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
 
     # Envoyer la requête et lire la réponse
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        # Lire le contenu de la réponse
-        data = response.read()
+	if response.readable():
+        	# Lire le contenu de la réponse
+        	data = response.read()
         
-        # Afficher le contenu de la réponse avec le format spécifié
-        print("Body response:")
-        print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
-        print("\t- utf8 content: {}".format(data.decode("utf-8")))
+        	# Afficher le contenu de la réponse avec le format spécifié
+        	print("Body response:")
+       		print("\t- type: {}".format(type(data)))
+        	print("\t- content: {}".format(data))
+        	print("\t- utf8 content: {}".format(data.decode("utf-8")))
 
