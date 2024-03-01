b5221bf886a2c843d16866fb28167b8c17d8296c
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-01 21:53:14 +0000

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,18 @@
 import urllib.request
 
 if __name__ == "__main__":
+    # Définir l'URL cible
     url = 'https://alx-intranet.hbtn.io/status'
 
-    with urllib.request.urlopen(url) as response:
+    # Créer une requête GET
+    req = urllib.request.Request(url, method='GET')
+
+    # Envoyer la requête et lire la réponse
+    with urllib.request.urlopen(req) as response:
+        # Lire le contenu de la réponse
         data = response.read()
+        
+        # Afficher le contenu de la réponse avec le format spécifié
         print("Body response:")
         print("\t- type: {}".format(type(data)))
         print("\t- content: {}".format(data))
