fb8e28115e162ef0a8568f2c7040347e5fb19f13
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-01 22:15:45 +0000

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""Script to fetch X-Request-Id variable from response header"""
-import urllib.request
+"""Fetches a header of a response from a URL."""
 import sys
+from urllib import request
 
-if __name__ == "__main__":
-    # Récupérer l'URL depuis les arguments de la ligne de commande
-    url = sys.argv[1]
 
-    # Créer une requête et envoyer la requête à l'URL spécifiée
-    with urllib.request.urlopen(url) as response:
-        # Récupérer la valeur de l'en-tête X-Request-Id
-        x_request_id = response.getheader('X-Request-Id')
-
-        # Afficher la valeur de l'en-tête X-Request-Id
-        print(x_request_id)
-
+if __name__ == '__main__':
+        url = sys.argv[1]
+        with request.urlopen(url) as response:
+            print(response.headers['X-Request-Id'])
