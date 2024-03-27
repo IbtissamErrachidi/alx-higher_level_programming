3fdbc1b94cb0ec024e8919c31b5a8c3aaf356136
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-27 14:31:22 -0700

    writeme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('File has been written successfully!');
+  }
+});
